181910dacfd9192004644ff4ce9e5a13dcec341a
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-07-07 14:39:21 -0600

    Desig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NewsModule } from './news/news.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NewsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
